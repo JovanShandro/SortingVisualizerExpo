6f8d40530dd5896e05a94a3000adbfc9ec058c32
Jovan Shandro <shandrojovan@gmail.com>
2020-10-05 10:03:22 +0200

    small demo of how animation will work

--- components/VisualizationCard.tsx
+++ components/VisualizationCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   TouchableOpacity,
@@ -17,10 +17,66 @@ interface Props {
 }
 
 const VisualizationCard: React.FC<Props> = ({ item, onPress, style }) => {
+  const array = [20, 35, 52, 27, 113, 64, 20, 35, 52, 27, 103, 64];
+  const animations = [
+    [0, 2],
+    [0, 2],
+    [0, 52]
+  ];
+  const [parameters, setParameters] = useState<any>(
+    array.map(value => ({
+      color: "lightseagreen",
+      height: value
+    }))
+  );
+
+  const handlePlay = () => {
+    animations.forEach((value, ind) => {
+      const m = ind % 3;
+      if (m === 0) {
+        setTimeout(() => {
+          setParameters((prev: any) => {
+            const copy = prev.slice();
+            copy[value[0]].color = "red";
+            copy[value[1]].color = "red";
+            return copy;
+          });
+        }, ind * 20);
+      } else if (m == 1) {
+        setTimeout(() => {
+          setParameters((prev: any) => {
+            const copy = prev.slice();
+            copy[value[0]].color = "lightseagreen";
+            copy[value[1]].color = "lightseagreen";
+            return copy;
+          });
+        }, ind * 40);
+      } else {
+        setTimeout(() => {
+          setParameters((prev: any) => {
+            const copy = prev.slice();
+            copy[value[0]].height = value[1];
+            return copy;
+          });
+        }, ind * 60);
+      }
+    });
+  };
+
   return (
     <TouchableOpacity style={style} activeOpacity={1} onPress={() => {}}>
       <Text style={[styles.header]}>{item.title}</Text>
-      <View style={styles.board}></View>
+      <View style={styles.board}>
+        {parameters.map((value: any, ind: number) => (
+          <View
+            key={ind}
+            style={[
+              styles.column,
+              { height: value.height, backgroundColor: value.color, width: 5 }
+            ]}
+          ></View>
+        ))}
+      </View>
       <View style={styles.buttons}>
         <Entypo style={styles.icon} name="shuffle" size={24} color="black" />
         <Entypo
@@ -28,6 +84,7 @@ const VisualizationCard: React.FC<Props> = ({ item, onPress, style }) => {
           name="controller-play"
           size={32}
           color="black"
+          onPress={handlePlay}
         />
         <Entypo
           style={styles.icon}
@@ -45,6 +102,7 @@ interface Style {
   header: TextStyle;
   board: ViewStyle;
   buttons: ViewStyle;
+  column: ViewStyle;
   icon: ViewStyle;
 }
 
@@ -57,8 +115,10 @@ const styles = StyleSheet.create<Style>({
   },
   board: {
     flex: 1,
-    backgroundColor: "lightseagreen",
-    marginHorizontal: "10%"
+    marginHorizontal: "10%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end"
   },
   buttons: {
     flexDirection: "row",
@@ -69,7 +129,8 @@ const styles = StyleSheet.create<Style>({
   },
   icon: {
     color: "#00000099"
-  }
+  },
+  column: {}
 });
 
 export default VisualizationCard;
