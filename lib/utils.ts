04d161ac518abec9998d7c074b1b027ddf65eccb
Jovan Shandro <shandrojovan@gmail.com>
2020-10-05 18:57:04 +0200

    change array length and fix problem with floats

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,22 @@ export const capitalize = (str: string) => {
 
 export const generateRandomArray = () => {
   const array = [];
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 25; i++) {
     array.push(randomNumber(7, 120));
   }
   return array;
 };
 
 const randomNumber = (min: number, max: number): number => {
-  return Math.random() * (max - min) + min;
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+export const swap = (
+  array: any[],
+  first_index: number,
+  second_index: number
+) => {
+  const tmp = array[first_index];
+  array[first_index] = array[second_index];
+  array[second_index] = tmp;
 };
