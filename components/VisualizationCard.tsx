cb5eb25e084003d651da9d9676be8fb9f6a645d0
Jovan Shandro <shandrojovan@gmail.com>
2020-10-05 09:37:38 +0200

    basic styling of visualization card

--- components/VisualizationCard.tsx
+++ components/VisualizationCard.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Text, TouchableOpacity, ViewStyle } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ViewStyle,
+  View,
+  TextStyle
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
 import { Algorithm } from "../constants";
 
 interface Props {
@@ -10,10 +18,58 @@ interface Props {
 
 const VisualizationCard: React.FC<Props> = ({ item, onPress, style }) => {
   return (
-    <TouchableOpacity style={style} activeOpacity={1} onPress={onPress}>
-      <Text>{item.title}</Text>
+    <TouchableOpacity style={style} activeOpacity={1} onPress={() => {}}>
+      <Text style={[styles.header]}>{item.title}</Text>
+      <View style={styles.board}></View>
+      <View style={styles.buttons}>
+        <Entypo style={styles.icon} name="shuffle" size={24} color="black" />
+        <Entypo
+          style={styles.icon}
+          name="controller-play"
+          size={32}
+          color="black"
+        />
+        <Entypo
+          style={styles.icon}
+          name="info-with-circle"
+          size={24}
+          color="black"
+          onPress={onPress}
+        />
+      </View>
     </TouchableOpacity>
   );
 };
+
+interface Style {
+  header: TextStyle;
+  board: ViewStyle;
+  buttons: ViewStyle;
+  icon: ViewStyle;
+}
+
+const styles = StyleSheet.create<Style>({
+  header: {
+    marginTop: 5,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 17
+  },
+  board: {
+    flex: 1,
+    backgroundColor: "lightseagreen",
+    marginHorizontal: "10%"
+  },
+  buttons: {
+    flexDirection: "row",
+    height: "20%",
+    justifyContent: "space-around",
+    alignItems: "center",
+    paddingHorizontal: "15%"
+  },
+  icon: {
+    color: "#00000099"
+  }
+});
 
 export default VisualizationCard;
